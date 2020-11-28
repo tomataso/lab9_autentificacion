a76c43469e9a9bb8a9090261880813a0b1278d7c
tomataso <tomataso@users.noreply.github.com>
2020-11-28 00:28:10 -0600

    Parte 2 (Ver Comentario)
    No pude encontrar la clase SecurityConfiguration por lo que la clave generada sigue apareciendo. No se en que parte del proyecto está. (Ver Paso 13)

---
+++ SeguridadSpring/src/main/java/com/cenfotec/seguridad/MemorySecurityConfiguration.java
@@ -0,0 +1,15 @@
+package com.cenfotec.seguridad;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class MemorySecurityConfiguration  extends WebSecurityConfigurerAdapter{
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		String encodedPassword = new BCryptPasswordEncoder().encode("mario");
+
+		auth.inMemoryAuthentication().withUser("mario").password(encodedPassword).roles("manager", "dev").and()
+				.passwordEncoder(new BCryptPasswordEncoder());
+	}
+}
