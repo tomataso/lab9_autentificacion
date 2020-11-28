b8579b1c60901dceffc694d0446f3483bdc3220c
tomataso <tomataso@users.noreply.github.com>
2020-11-28 00:18:43 -0600

    Parte 1

---
+++ SeguridadSpring/src/main/java/com/cenfotec/seguridad/SeguridadSpringApplication.java
@@ -0,0 +1,13 @@
+package com.cenfotec.seguridad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeguridadSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeguridadSpringApplication.class, args);
+	}
+
+}
